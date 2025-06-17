f1fed9e28212ee57a4ab5135ed11828c63510e08
Ana Kochievi <anna.kochievi@gmail.com>
2025-06-17 14:30:02 +0400

    ToDoList app added to repository.

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Techzy on 17.06.25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Techzy on 17.06.25.
+//
+
+import Testing
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
